6c74b8ef0c6068b58b344d3727367c34e87b9e47
Emily Yonker <emilyfyonker@protonmail.com>
2019-06-11 16:15:45 -0400

    handlebars, jquery, set up mongoose connection

---
+++ models/Note.js
@@ -0,0 +1,22 @@
+console.log('inside Note.js');
+
+// Require mongoose
+var mongoose = require("mongoose");
+// Create a schema class
+var Schema = mongoose.Schema;
+
+// Create the Note schema
+var NoteSchema = new Schema({
+  // a string
+  title: String,
+  // `body` is of type String
+  body: String
+});
+
+
+
+// This creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
